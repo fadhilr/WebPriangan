3980fc81cddbe5c617a336f1f98e2480580e73d9
Fadhil Rizqullah Saniputra <fadilrizqullah97@gmail.com>
2019-11-26 15:31:44 +0700

    carousel

--- application/views/tampilanAwal.php
+++ application/views/tampilanAwal.php
@@ -18,10 +18,14 @@
 			window.scrollTo(0, 1);
 		}
 	</script>
+	
+	<!-- slider -->
+	
+	<!-- //slider -->
 	<!--// Meta tag Keywords -->
 
     <!-- Custom-Files -->
-    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?php echo base_url();?>/assets/css/bootstrap.css">
 	<!-- Bootstrap-Core-CSS -->
 	<link href="<?php echo base_url();?>/assets/css/css_slider.css" type="text/css" rel="stylesheet" media="all">
@@ -138,6 +142,7 @@
 	<!-- //header 2 -->
 
 	<!-- banner -->
+	
 	<div class="baneer-w3ls">
 		<div class="row no-gutters">
 			<div class="col-xl-12 col-lg-6">
@@ -161,35 +166,31 @@
 		<div class="row no-gutters">
 			<div class="col-xl-12 col-lg-6 callbacks_container">
 				<!-- banner slider -->
-				<div class="csslider infinity" id="slider1">
-					<input type="radio" name="slides" checked="checked" id="slides_1" />
-					<input type="radio" name="slides" id="slides_2" />
-					<input type="radio" name="slides" id="slides_3" />
-					<ul class="banner_slide_bg">
-					<?php 
-						foreach($banner as $a){ 
-					?>
-						<li>
-							<div class="">
-							<img src="<?php echo base_url();?>/assets/images/foto/<?php echo "$a[fotoBanner]"?>" class="" alt="User Image" width="100%" height="600px">
-							</div>
-						</li>
-					<?php 
-						} 
-					?>
-					</ul>
-					<div class="arrows">
-						<label for="slides_1"></label>
-						<label for="slides_2"></label>
-						<label for="slides_3"></label>
-					</div>
-					<div class="navigation">
-						<div>
-							<label for="slides_1"></label>
-							<label for="slides_2"></label>
-							<label for="slides_3"></label>
+				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						
+						<div class="carousel-item active">
+							<img src="<?php echo base_url();?>/assets/images/Banner1.jpg" class="" alt="User Image" width="100%" height="600px">
 						</div>
+						<?php foreach($banner as $a) { ?>
+						<div class="carousel-item">
+							<img src="<?php echo base_url();?>/assets/images/foto/<?php echo "$a[fotoBanner]"?>" class="" alt="User Image" width="100%" height="600px">
+						</div>
+						<?php } ?>
 					</div>
+					<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
 				</div>
 				<!-- //banner slider -->
 			</div>
@@ -335,6 +336,22 @@
 	</a>
 	<!-- //move top icon -->
 
+
+	<!-- js -->
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script>
+			$('.carousel').carousel({
+			interval: 2000
+			});
+	</script>	
+<!-- //js -->
+		
 </body>
+
+
+
 
 </html>
